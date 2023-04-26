34f3b4e1a171a63f3b0a312e7573c0057cf98ea3
Saad Ezzini <saad.ezzini@hotmail.com>
2023-04-26 15:51:31 +0200

    Update sqam.py
    Improved conditions matching

--- sqam.py
+++ sqam.py
@@ -1,8 +1,8 @@
 import re
 
 def split_sql_query(query):
-    query = query.strip()
-    
+    query = query.replace(';','').strip()
+
     # extract SELECT statement
     select_end = query.find(' FROM ')
     select_clause = query[:select_end] if select_end != -1 else query
@@ -18,33 +18,32 @@ def split_sql_query(query):
     where_start = from_end + 7 if ' WHERE ' in query else len(query)
     where_end = query.find(' GROUP BY ') if ' GROUP BY ' in query else len(query)
     where_clause = query[where_start:where_end].strip()
-    where_items = [item.strip() for item in re.split(r'\s+(?:AND|OR)\s+', where_clause, flags=re.IGNORECASE)] if where_clause != '' else None
-    
+    where_items = [re.sub('[' +  ''.join(['\'',' ','"']) +  ']', '', item).lower().split('.')[-1] for item in re.split(r'\s+(?:AND|OR)\s+', where_clause, flags=re.IGNORECASE)] if where_clause != '' else None
+
     # extract GROUP BY statement
     group_start = where_end + 10 if ' GROUP BY ' in query else len(query)
     group_end = query.find(' HAVING ') if ' HAVING ' in query else len(query)
     group_clause = query[group_start:group_end].strip()
     group_items = [item.strip() for item in group_clause.split(',') if item.strip()] if group_clause != '' else None
-    
+
     # extract HAVING conditions
     having_start = group_end + 8 if ' HAVING ' in query else len(query)
     having_end = query.find(' ORDER BY ') if ' ORDER BY ' in query else len(query)
     having_clause = query[having_start:having_end].strip()
     having_items = [item.strip() for item in re.split(r'\s+(?:AND|OR)\s+', having_clause, flags=re.IGNORECASE)] if having_clause != '' else None
-    
+
     # extract ORDER BY statement
     order_start = having_end + 10 if ' ORDER BY ' in query else len(query)
     order_end = len(query)
     order_clause = query[order_start:order_end].strip()
     order_items = [item.strip() for item in order_clause.split(',') if item.strip()] if order_clause != '' else None
-    
+
     # extract LIMIT number
-    
+
     limit_start = query.find(' LIMIT ') + 7 if ' LIMIT ' in query else len(query)
     limit_clause = query[limit_start:].strip()
     limit_number = int(limit_clause) if limit_clause.isdigit() else None
 
-    
     # return dictionary of subitems
     return {'SELECT': select_items, 'FROM': from_items, 'WHERE': where_items, 
             'GROUP BY': group_items, 'HAVING': having_items, 'ORDER BY': order_items, 'LIMIT': [limit_number]}
@@ -87,7 +86,7 @@ def sql_query_accuracy(query, true_query):
 
 
     # calculate the accuracy score as the percentage of matching subitems
-   
+
     if total_count == 0:
         accuracy_score = 0
     else:
@@ -96,13 +95,13 @@ def sql_query_accuracy(query, true_query):
     return accuracy_score
   
 def sqam_batch(y_list,gt_list):
-    
+
     if len(y_list)!= len(gt_list):
         print('ERROR: the input lists should have the same length')
         return
     score=0
-    
+
     for y,gt in zip(y_list,gt_list):
         score+=sql_query_accuracy(y,gt)
-    
+
     return score / len(y_list)
